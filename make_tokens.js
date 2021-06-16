ee0abb820b1f4da2c991cee3734ce4bb54035766
Akito Fujita <akito_fujita@mvg.biglobe.ne.jp>
2021-06-17 03:18:13 +0900

    use map instead of Associative array

--- make_tokens.js
+++ make_tokens.js
@@ -38,43 +38,43 @@ function preprocess_content(text)
  *
  */
 const fs = require('fs');
-console.log("cwd: %s", process.cwd());
-console.log("path: %s", process.argv[2]);
+//console.log("cwd: %s", process.cwd());
+//console.log("path: %s", process.argv[2]);
 
 var file = fs.readFileSync(process.cwd()+"/"+process.argv[2], 'utf-8');
 var json = JSON.parse(file);
 var text = preprocess_content(json.text);
 
-var token2id = {};
+var map = new Map();
 var id2token = [];
 var id2parag = [];
 var paragraphs = text.split('\n');
-for (var i = 0; i < paragraphs.length; i++) {
-  //console.log("# %d: %s", i, paragraphs[i]);
+for (var i = 0, n = 0; i < paragraphs.length; i++) {
   var tokens = paragraphs[i].split(' ');
-  var para = [];
-  for (var j = 0; j < tokens.length; j++) {
-    //process.stdout.write(" "+tokens[j]);
+  var paragr = [];
+  for (var j = 0; j < tokens.length; j++, n++) {
+    //console.log("%d: %s", n, tokens[j]);
     var id;
-    if (token2id[tokens[j]]) {
-      id = token2id[tokens[j]];
+    if (map.has(tokens[j])) {
+      id = map.get(tokens[j]);
     } else {
       id = id2token.length;
-      token2id[tokens[j]] = id;
+      map.set(tokens[j], id);
       id2token.push(tokens[j]);
     }
-    para.push(id);
+    paragr.push(id);
   }
-  id2parag.push(para);
+  id2parag.push(paragr);
 }
-/*
-var keys = Object.keys(token2id);
-for (const key of keys) {
-  console.log("key: %s, id: %d", key, token2id[key]);
-}
-*/
+
+var token2id = {};
+map.forEach(function(v, k){
+  //console.log("k: %s, v: %d", k, v);
+  token2id[k] = v;
+});
 //console.log(JSON.stringify(token2id, undefined, 2));
-//console.log(JSON.stringify(id2parag, undefined, 2));
+//console.log(JSON.stringify(id2token, undefined, 2));
+
 fs.writeFile("wikipedia-tokens.json",
 	     JSON.stringify(token2id, undefined, 2), 
 	     function(err, result) {
@@ -85,3 +85,4 @@ fs.writeFile("wikipedia-papers.json",
 	     function(err, result) {
 	       if (err) console.log('error', err);
 	     });
+
